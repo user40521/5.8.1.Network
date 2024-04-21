4442792112d5c234ebc8eca5e126d360d44945e1
Neto MurashovVV <vla-murashov@yandex.ru>
2024-04-21 20:57:13 +0300

    Initial commit. Gitignore added

---
+++ src/main/java/MainServer.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainServer {
+    public static void main(String[] args) {
+        System.out.println("Server started...");
+        int port = 8087;
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+            {
+                System.out.printf("New connection accepted Port: %d%n", clientSocket.getPort());
+                final String name = in.readLine();
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
